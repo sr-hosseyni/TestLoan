f7824a3756c0b33b415b6f0f994ee2c80480d7b2
sr_hosseini <rassoul.hosseini@insly.com>
2019-10-01 00:33:20 +0300

    Generate CRUD for Loan with gii

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,6 +40,7 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'Users', 'url' => ['/user/index']],
+            ['label' => 'Loans', 'url' => ['/loan/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
---
+++ views/loan/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\LoanSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Loans';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="loan-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Loan', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'user_id',
+            'amount',
+            'interest',
+            'duration',
+            //'start_date',
+            //'end_date',
+            //'campaign',
+            //'status:boolean',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
